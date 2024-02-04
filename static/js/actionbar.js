10631a7c99dd8c74c0f0c3e39b81349aede05aaa
naz-iv <nazarivankiv1@gmail.com>
2024-02-04 17:41:25 +0200

    added animations to content

--- static/js/actionbar.js
+++ static/js/actionbar.js
@@ -400,3 +400,17 @@
     }
 }
 )();
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show")
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll(".hide-left, .hide-right")
+hiddenElements.forEach((el) => observer.observe(el));
