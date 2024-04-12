d9d3ae89f04f430e6e47a8ea59ce0737ac1bd241
naz-iv <nazarivankiv1@gmail.com>
2024-04-12 14:31:42 +0300

    updated static files handling

--- core/settings.py
+++ core/settings.py
@@ -95,6 +95,11 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
